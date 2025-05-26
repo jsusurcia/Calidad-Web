494de160be51b78232037cb8391fcc6337ced7fe
jsusurcia <jnthnur-gmail.com>
2025-05-26 10:37:47 -0500

    validación completa d incidencia cliente

--- js/validaciones-incidente-cliente.js
+++ js/validaciones-incidente-cliente.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const fechaIncidente = document.getElementById('fecha-incidente-cliente');
     const tipoDocRadios = document.querySelectorAll('input[name="tipo-doc"]');
     const numDocumentoInput = document.getElementById('num-documento-cliente');
     const sucursalesSelect = document.getElementById('sucursales');
@@ -6,9 +7,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const empleadoSelect = document.getElementById('empleado');
     const evidenciaInput = document.getElementById('evidencia');
     
+    // Asignar fecha actual al campo de fecha del incidente
+    const actual = new Date().toISOString().split('T')[0];
+    fechaIncidente.value = actual;
+
     // Validación de tipo de documento (habilitar el número de documento cuando se seleccione)
     tipoDocRadios.forEach(radio => {
         radio.addEventListener('change', function () {
+            numDocumentoInput.value = '';
             numDocumentoInput.disabled = false;
         });
     });
@@ -16,11 +22,15 @@ document.addEventListener('DOMContentLoaded', function () {
     // Validación de sucursal (habilitar área cuando se seleccione una sucursal)
     sucursalesSelect.addEventListener('change', function () {
         areasSelect.disabled = false;
+        areasSelect.selectedIndex = 0;
+        empleadoSelect.disabled = true;
+        empleadoSelect.selectedIndex = 0;
     });
 
     // Validación de área (habilitar empleado cuando se seleccione un área)
     areasSelect.addEventListener('change', function () {
         empleadoSelect.disabled = false;
+        empleadoSelect.selectedIndex = 0;
     });
 
     // Validación para aceptar solo imágenes en el campo de archivo
